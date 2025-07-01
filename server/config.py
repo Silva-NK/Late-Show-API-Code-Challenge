d567a5a6df25c77b43b290956d96fca64c1b8f6c
Silva-NK <kiagonsuzanne@gmail.com>
2025-07-01 06:51:57 +0300

    Rework everything.

--- server/config.py
+++ server/config.py
@@ -1,12 +1,12 @@
-# import os
+import os
 
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
-# SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-# SQLALCHEMY_TRACK_MODIFICATIONS = False
-# JSON_COMPACT = False
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+JSON_COMPACT = False
     
-# JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-# JWT_ACCESS_TOKEN_EXPIRES = int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES", 3600))
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+JWT_ACCESS_TOKEN_EXPIRES = int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES", 3600))
